87525558cfba004a2a4a423da09c9d2baa8bf796
Carlos Camilo <carloscamilocontato@gmail.com>
2025-10-04 23:51:00 -0300

    rows

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <assert.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -39,6 +40,15 @@ MetaCommandResult parse_meta_command(const InputBuffer* input_buffer) {
     return META_COMMAND_UNKNOWN_COMMAND;
 }
 
+#define COLUMN_USERNAME_SIZE 32
+#define COLUMN_EMAIL_SIZE 255
+
+typedef struct {
+    uint32_t id;
+    char username[COLUMN_USERNAME_SIZE];
+    char email[COLUMN_EMAIL_SIZE];
+} Row;
+
 typedef enum {
     STATEMENT_SELECT,
     STATEMENT_INSERT,
@@ -46,10 +56,12 @@ typedef enum {
 
 typedef struct {
     StatementType type;
+    Row row_to_insert;
 } Statement;
 
 typedef enum {
     PREPARE_SUCCESS,
+    PREPARE_SYNTAX_ERROR,
     PREPARE_UNRECOGNIZED_STATEMENT,
 } PrepareResult;
 
@@ -61,6 +73,19 @@ PrepareResult prepare_statement(const InputBuffer* input_buffer, Statement* stat
 
     if (strncmp(input_buffer->buffer, "INSERT", 6) == 0) {
         statement->type = STATEMENT_INSERT;
+
+        const int args = sscanf(
+            input_buffer->buffer,
+            "INSERT %d %s %s",
+            &(statement->row_to_insert.id),
+            (char*)(&(statement->row_to_insert.username)),
+            (char*)(&(statement->row_to_insert.email))
+        );
+
+        if (args < 3) {
+            return PREPARE_SYNTAX_ERROR;
+        }
+
         return PREPARE_SUCCESS;
     }
 
@@ -77,6 +102,40 @@ void execute_statement(const Statement* statement) {
             break;
     }
 }
+
+#define SIZE_OF_ATTRIBUTE(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
+
+const uint32_t ID_SIZE = SIZE_OF_ATTRIBUTE(Row, id);
+const uint32_t USERNAME_SIZE = SIZE_OF_ATTRIBUTE(Row, username);
+const uint32_t EMAIL_SIZE = SIZE_OF_ATTRIBUTE(Row, email);
+
+const uint32_t ID_OFFSET = 0;
+const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
+const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
+
+const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
+
+void row_serialize(const Row* row, void* dest) {
+    memcpy(dest + ID_OFFSET, &(row->id), ID_SIZE);
+    memcpy(dest + USERNAME_OFFSET, &(row->username), USERNAME_SIZE);
+    memcpy(dest + EMAIL_OFFSET, &(row->email), EMAIL_SIZE);
+}
+
+void row_deserialize(const void* source, Row* row) {
+    memcpy(&(row->id), source + ID_OFFSET, ID_SIZE);
+    memcpy(&(row->username), source + USERNAME_OFFSET, USERNAME_SIZE);
+    memcpy(&(row->email), source + EMAIL_OFFSET, EMAIL_SIZE);
+}
+
+#define TABLE_MAX_PAGES 100
+const uint32_t PAGE_SIZE = 4096;
+const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
+const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
+
+typedef struct {
+    size_t num_rows;
+    void* pages[TABLE_MAX_PAGES];
+} Table;
 
 int main(void) {
     InputBuffer input_buffer = input_buffer_init();
@@ -104,6 +163,8 @@ int main(void) {
                 break;
             case PREPARE_UNRECOGNIZED_STATEMENT:
                 printf("Unrecognized statement at start of: '%s'\n", input_buffer.buffer);
+                continue;
+            case PREPARE_SYNTAX_ERROR:
                 continue;
         }
 
